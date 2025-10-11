74a8ba43f6a33e1873cef1d37df17f59340c459a
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2025-10-11 14:48:26 -0300

    feat : spring project

---
+++ project/backend/src/test/java/com/sistema/moedaEstudantil/MoedaEstudantilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.moedaEstudantil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoedaEstudantilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
