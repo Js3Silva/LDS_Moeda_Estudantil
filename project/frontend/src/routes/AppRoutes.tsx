973491c9ad31ff90a67351f78235850530da6ae4
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2025-10-16 11:26:56 -0300

    fix : nomenclatura

--- project/frontend/src/routes/AppRoutes.tsx
+++ project/frontend/src/routes/AppRoutes.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import HomePage from "../pages/Home/HomePage";
 import EmpresaList from "../pages/Empresas/EmpresaList";
 import EmpresaForm from "../pages/Empresas/EmpresaForm";
-// import ClienteList from "../pages/Clientes/ClienteList";
-// import ClienteForm from "../pages/Clientes/ClienteForm";
+// import AlunoList from "../pages/Clientes/AlunoList";
+// import AlunoForm from "../pages/Clientes/AlunoForm";
 
 export default function AppRoutes() {
   return (
@@ -17,9 +17,9 @@ export default function AppRoutes() {
         <Route path="/empresas/:id" element={<EmpresaForm />} />
         
         {/* Rotas de Clientes */}
-        {/* <Route path="/clientes" element={<ClienteList />} />
-        <Route path="/clientes/novo" element={<ClienteForm />} />
-        <Route path="/clientes/:id" element={<ClienteForm />} /> */}
+        {/* <Route path="/clientes" element={<AlunoList />} />
+        <Route path="/clientes/novo" element={<AlunoForm />} />
+        <Route path="/clientes/:id" element={<AlunoForm />} /> */}
       </Routes>
     </BrowserRouter>
   );
