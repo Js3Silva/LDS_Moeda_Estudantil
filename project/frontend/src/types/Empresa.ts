52b079ea7fa9a0409ec0ffb3c08f1bdfd61d4f61
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2025-10-17 09:22:48 -0300

    fix : integração com backend

--- project/frontend/src/types/Empresa.ts
+++ project/frontend/src/types/Empresa.ts
@@ -1,4 +1,21 @@
+
+export interface EmpresaCreate {
+  id: number;
+  nome: string;
+  email: string;
+  cnpj: string;
+  senha: string;
+}
+
 export interface Empresa {
+  id: number;
+  nome: string;
+  email: string;
+  cnpj: string;
+  senha: string;
+}
+
+export interface EmpresaUpdate {
   id: number;
   nome: string;
   email: string;
