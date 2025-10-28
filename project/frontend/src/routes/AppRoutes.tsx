344f7691f10c66a9006caf1dcbf2234e4e41e3af
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2025-10-28 18:24:22 -0300

    fix: rotas da home

--- project/frontend/src/routes/AppRoutes.tsx
+++ project/frontend/src/routes/AppRoutes.tsx
@@ -6,24 +6,39 @@ import CadastroAlunoPage from "../pages/Aluno/CadastroAlunoPage";
 import PerfilAlunoPage from "../pages/Aluno/PerfilAlunoPage";
 import AlunoList from "../pages/Aluno/AlunoList";
 import Layout from "./Layout";
+// import HomePageAluno from "../pages/Home/HomePageAluno";
+// import HomePageEmpresa from "../pages/Home/HomePageEmpresa";
 
 export default function AppRoutes() {
+  const user = localStorage.getItem("user");
+
+  // 🔹 Escolhe a página inicial conforme o tipo de usuário
+  let homeElement;
+  if (user === "admin") {
+    homeElement = <HomePage />;
+  } else if (user === "aluno") {
+    // homeElement = <HomePageAluno />;
+  } else if (user === "empresa") {
+    // homeElement = <HomePageEmpresa />;
+  } else {
+    homeElement = <LoginPage />;
+  }
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<LoginPage />} />
+          {/* Página inicial dinâmica */}
+          <Route index element={homeElement} />
 
           {/* Rotas de Empresas */}
           <Route path="empresas" element={<EmpresaList />} />
 
           {/* Rotas de Alunos */}
-          <Route path="home" element={<HomePage />} />
           <Route path="aluno/novo" element={<CadastroAlunoPage />} />
           <Route path="aluno" element={<PerfilAlunoPage />} />
           <Route path="alunos" element={<AlunoList />} />
           <Route path="login" element={<LoginPage />} />
-
         </Route>
       </Routes>
     </BrowserRouter>
